19121b1f31b584691e8dc1b8a9caaa0604455673
Renée Kooi <renee@kooi.me>
2020-11-03 10:34:44 +0100

    use separate collections on CI runs

--- test.js
+++ test.js
@@ -7,7 +7,15 @@ test.before((t) => {
   t.truthy(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'GOOGLE_APPLICATION_CREDENTIALS must be set')
 })
 
+let collection = 'keyv-firestore'
+// Use a unique-er collection name for concurrent CI runs
+if (process.env.GITHUB_ACTIONS) {
+  const keyvVersion = require('keyv/package.json').version
+  const nodeVersion = process.version
+  collection = `keyv-firestore-gh${process.env.GITHUB_RUN_ID}-${keyvVersion}-${nodeVersion}`
+}
+
 testSuite(test, Keyv, () => new KeyvFirestore({
   projectId: 'uwave-demo',
-  collection: 'keyv-firestore'
+  collection
 }))
