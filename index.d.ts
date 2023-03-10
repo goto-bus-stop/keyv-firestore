00c119fafcfb1cf9ede8c70eee48b3fe100831b3
Kid <44045911+kidonng@users.noreply.github.com>
2023-03-10 13:23:03 +0800

    Add comments

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,10 @@
 import {Store} from 'keyv'
 
+// Not precise, but typing the entire class seems overkill
 declare const keyvFirestore: new (options: {
   projectId?: string
   collection: string
+  // https://github.com/googleapis/nodejs-firestore/blob/207c9359e369b6838f026fd62ff56df39332dcff/dev/src/index.ts#L513-L518
   credentials?: {
     client_email: string
     private_key: string
