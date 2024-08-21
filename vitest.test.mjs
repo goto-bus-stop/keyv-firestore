9e20cc479f45d0db63d6bd6d232918d108adfa23
Renée <renee@kooi.me>
2024-08-21 22:24:29 +0200

    Support keyv@5.x (#52)
    * ci: add keyv 5
    
    * only run keyv 5 on recent node versions
    
    * ci: provide version to test by env var
    
    * ci: use vitest for keyv v5
    
    * propagate versions; provide defaults
    
    * YEP
    
    * Fix vitest runner
    
    * lint
    
    * explicit file import
    
    * fix vitest runner, v2
    
    * ci: only run ava test with ava
    
    * document compatibility

---
+++ vitest.test.mjs
@@ -0,0 +1,24 @@
+import assert from 'node:assert'
+import { readFileSync } from 'node:fs'
+import * as vitest from 'vitest'
+import testSuite from '@keyv/test-suite'
+import Keyv from 'keyv'
+import KeyvFirestore from './index.js'
+
+const keyvVersion = JSON.parse(readFileSync(new URL('./node_modules/keyv/package.json', import.meta.url), 'utf8')).version
+const nodeVersion = process.version
+
+vitest.beforeAll(() => {
+  assert(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'GOOGLE_APPLICATION_CREDENTIALS must be set')
+})
+
+let collection = 'keyv-firestore'
+// Use a unique-er collection name for concurrent CI runs
+if (process.env.GITHUB_ACTIONS) {
+  collection = `keyv-firestore-gh${process.env.GITHUB_RUN_ID}-${keyvVersion}-${nodeVersion}`
+}
+
+testSuite(vitest, Keyv, () => new KeyvFirestore({
+  projectId: 'uwave-demo',
+  collection
+}))
