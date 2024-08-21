53e61273f610580da40653fbdcbd40f800395fee
Renée Kooi <renee@kooi.me>
2024-08-21 21:40:11 +0200

    ci: use vitest for keyv v5

--- test.js
+++ test.js
@@ -1,19 +1,25 @@
 'use strict'
 
-const test = require('ava')
+const keyvVersion = process.env.KEYV_VERSION
+const suiteVersion = process.env.KEYV_SUITE_VERSION
+const nodeVersion = process.version
+
+// Test suite >= 2 use vitest instead of ava.
+const assert = require('assert')
+const test = require(Number(suiteVersion.split('.')[0]) < 2 ? 'ava' : 'vitest')
 const testSuite = require('@keyv/test-suite').default
 const Keyv = require('keyv')
 const KeyvFirestore = require('.')
 
-test.before((t) => {
-  t.truthy(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'GOOGLE_APPLICATION_CREDENTIALS must be set')
+// ava: before, vitest: beforeAll
+const before = test.before || test.beforeAll
+before(() => {
+  assert(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'GOOGLE_APPLICATION_CREDENTIALS must be set')
 })
 
 let collection = 'keyv-firestore'
 // Use a unique-er collection name for concurrent CI runs
 if (process.env.GITHUB_ACTIONS) {
-  const keyvVersion = process.env.KEYV_VERSION
-  const nodeVersion = process.version
   collection = `keyv-firestore-gh${process.env.GITHUB_RUN_ID}-${keyvVersion}-${nodeVersion}`
 }
 
